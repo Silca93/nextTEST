f2f1c5d7b1489e68f33e6e31a31d2f6c1f5de741
Silca93 <eric_delhaye@hotmail.com>
2024-03-20 11:20:48 +0100

    setup

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,21 @@
-export default function Page (){
+import { fetchLatestInvoices, fetchRevenue } from "../lib/data";
+import RevenueChart from "../ui/dashboard/revenue-chart";
+import { RevenueChartSkeleton } from "../ui/skeletons";
+import LatestInvoices from "../ui/dashboard/latest-invoices";
+import { Suspense } from "react";
+
+export default async function Page () {
+    
+
+    const lastestInvoices = await fetchLatestInvoices()
+    
     return (
 
-        <div><p>Dashboard...</p></div>
+        <div className="flex">
+            <Suspense fallback={<RevenueChartSkeleton/>}>
+                <RevenueChart/>
+            </Suspense>
+            <LatestInvoices latestInvoices={lastestInvoices}/>
+        </div>
     )
 }
